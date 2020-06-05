c9c1aff585732c7953409383f8401aada393f4cb
haarinn <62758424+haarinn@users.noreply.github.com>
2020-06-06 00:08:13 +0900

    Update dbconfig.js

--- checkServer/config/dbconfig.js
+++ checkServer/config/dbconfig.js
@@ -2,10 +2,10 @@ const mysql = require('promise-mysql');
 
 const dbConfig = {
     host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'gkgk1234',
-    database: 'checkcheck', //db schema이름
+    port: ,
+    user: '',
+    password: '',
+    database: '', //db schema이름
 }
 
 module.exports = mysql.createPool(dbConfig);
